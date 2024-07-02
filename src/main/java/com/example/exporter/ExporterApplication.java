b0fa0fb1485424ada8f7a79accaace14b5970ef9
b-mazurkiewicz <b.mazurkiewicz@wasko.pl>
2024-07-02 14:00:25 +0200

    H2 DB update

--- src/main/java/com/example/exporter/ExporterApplication.java
+++ src/main/java/com/example/exporter/ExporterApplication.java
@@ -9,5 +9,10 @@ public class ExporterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExporterApplication.class, args);
 	}
-
+	CREATE TABLE tutorials_tbl (
+			id INT NOT NULL,
+			title VARCHAR(50) NOT NULL,
+	author VARCHAR(20) NOT NULL,
+	submission_date DATE
+);
 }

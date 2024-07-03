6269237da8afe75e0c18b5800d81f11202330cec
Marysia <okuniewicz13@gmail.com>
2024-07-03 08:15:40 +0200

    Add dependencies

--- src/main/java/com/example/exporter/ExporterApplication.java
+++ src/main/java/com/example/exporter/ExporterApplication.java
@@ -6,13 +6,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExporterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExporterApplication.class, args);
-	}
-	CREATE TABLE tutorials_tbl (
-			id INT NOT NULL,
-			title VARCHAR(50) NOT NULL,
-	author VARCHAR(20) NOT NULL,
-	submission_date DATE
-);
+
 }

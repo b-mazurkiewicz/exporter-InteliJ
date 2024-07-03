695bf3421612ad0c25e1bcad2945d43811d1ef49
Marysia <okuniewicz13@gmail.com>
2024-07-03 09:05:37 +0200

    Add model, controller, services classes

---
+++ src/main/java/com/example/exporter/User.java
@@ -0,0 +1,20 @@
+package com.example.exporter;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
-CREATE TABLE IF NOT EXISTS user (
-                                    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-    );
+CREATE TABLE user (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      first_name VARCHAR(255) NOT NULL,
+                      last_name VARCHAR(255) NOT NULL,
+                      email VARCHAR(255) NOT NULL
+);

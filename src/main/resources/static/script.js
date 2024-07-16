d44b6de3e7c25b7c926b362be8781f292a9c8475
b-mazurkiewicz <b.mazurkiewicz@wasko.pl>
2024-07-16 10:46:50 +0200

    checkboxes added (even working lol)
    idontwannaliveanymore

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,15 +1,26 @@
-document.getElementById('fetchTables').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function()
+{
     fetch('/api/tables')
         .then(response => response.json())
         .then(data => {
             const tableList = document.getElementById('tableList');
-            tableList.innerHTML = '';
+            tableList.innerHTML = ''; // Wyczyść listę przed dodaniem nowych elementów
             data.forEach(table => {
                 const li = document.createElement('li');
-                li.textContent = table;
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = table;
+                checkbox.name = 'tables';
+                checkbox.value = table;
+
+                const label = document.createElement('label');
+                label.htmlFor = table;
+                label.textContent = table;
+
+                li.appendChild(checkbox);
+                li.appendChild(label);
                 tableList.appendChild(li);
             });
         })
-        .catch(error => console.log('Error fetching tables: ', error));
-
+        .catch(error => console.error('Error fetching tables:', error));
 });

72cf01d6a2b054668834a063d7a6329a2101857f
b-mazurkiewicz <b.mazurkiewicz@wasko.pl>
2024-07-24 09:02:52 +0200

    init db save

---
+++ src/main/java/com/example/exporter/repository/FileDBRepository.java
@@ -0,0 +1,8 @@
+package com.example.exporter.repository;
+
+import com.example.exporter.model.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+}

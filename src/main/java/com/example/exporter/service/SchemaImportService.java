1eedbd45d6d9aa6ac21ae0cfd692a81a4b2f6a01
b-mazurkiewicz <b.mazurkiewicz@wasko.pl>
2024-07-31 14:00:18 +0200

    dodanie drzewa do sortowania danych

--- src/main/java/com/example/exporter/service/SchemaImportService.java
+++ src/main/java/com/example/exporter/service/SchemaImportService.java
@@ -1,6 +1,7 @@
 package com.example.exporter.service;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.logging.Logger;
 
 @Service
 public class SchemaImportService {
@@ -21,7 +20,11 @@ public class SchemaImportService {
     private final List<String> sheetNames = new ArrayList<>(); // Lista nazw arkuszy
 
     public Map<String, List<String>> readTableAndColumnNames(MultipartFile file) throws IOException {
+        boolean isSorted = false;
+
         Map<String, List<String>> tableColumnMap = new LinkedHashMap<>();
+        TreeMap<Integer, String> sortedMap = new TreeMap<>();
+
         firstRows.clear();
         sheetNames.clear(); // Czyść listę nazw arkuszy przed rozpoczęciem nowego importu
 
@@ -44,8 +47,9 @@ public class SchemaImportService {
                 // Odczytaj pierwszy i drugi wiersz
                 Row headerRow = sheet.getRow(0); // W pierwszym wierszu znajdują się nazwy, które użytkownik chce przepisać
                 Row dataRow = sheet.getRow(1); // W drugim wierszu znajduje się lokalizacja danych w bazie
+                Row sortingRow = sheet.getRow(2); // W trzecim wierszu znajdują się informacje potrzebne do sortowania danych
 
-                if (dataRow == null || headerRow == null) {
+                if (dataRow == null || headerRow == null || sortingRow == null) {
                     continue; // Przejdź do następnego arkusza, jeśli wiersz jest pusty
                 }
 
@@ -69,6 +73,20 @@ public class SchemaImportService {
                     if (cell.getCellType() == CellType.STRING) {
                         String cellValue = cell.getStringCellValue();
                         firstRow.add(cellValue);
+                    }
+                }
+
+                //sortingRow
+                for (int colIndex = 0; colIndex < sortingRow.getLastCellNum(); colIndex++) {
+                    Cell sortingCell = sortingRow.getCell(colIndex);
+                    Cell dataCell = dataRow.getCell(colIndex);
+
+                    if (sortingCell != null && sortingCell.getCellType() == CellType.NUMERIC) {
+                        int cellKey = (int) sortingCell.getNumericCellValue();
+                        String cellValue = dataCell != null ? dataCell.getStringCellValue() : "";
+
+                        sortedMap.put(cellKey, cellValue);
+                        isSorted = true;
                     }
                 }
             }
